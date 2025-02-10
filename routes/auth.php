330cf5bdd683d6b0ff5e2193570c7a5aa944df21
adib918 <adibdev.sbahi@gmail.com>
2025-02-11 00:49:19 +0300

    okay6

--- routes/auth.php
+++ routes/auth.php
@@ -29,9 +29,9 @@ Route::post( '/verify-email', function(VerifyEmailRequest $request){
     if(auth()->user()->email_verification_code != (int)$request->email_verification_code){
         return response()->json(["message" => "Verification code isn't correct."], 400);
     }
-    if(Carbon::now()->diffInMinutes(auth()->user()->email_verification_code_expire_at) > 60){
-        return response()->json(['message' => 'Your verification code has expired.'])->setStatusCode(400);
-    }
+    // if(Carbon::now()->diffInMinutes(auth()->user()->email_verification_code_expire_at) > 60){
+    //     return response()->json(['message' => 'Your verification code has expired.'])->setStatusCode(400);
+    // }
     if($user->hasVerifiedEmail()){
         return response()->json(['message' => 'Your email is already verified.'], 400);
     }
