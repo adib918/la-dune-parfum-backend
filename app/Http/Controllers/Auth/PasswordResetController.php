02ff5396f86a7ef37b8667dd50f8b4cbf9c166a8
adib918 <adibdev.sbahi@gmail.com>
2025-02-11 02:30:31 +0300

    okay17

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ResetPasswordRequest;
 use App\Http\Requests\updatePasswordRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Password;
 use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
@@ -39,9 +40,12 @@ class PasswordResetController extends Controller
             }
         );
 
+        $token = $request->token;
+
         return $status === Password::PASSWORD_RESET
                     ? response()->json([
                         'message' => 'Password successfully rest',
+                        'token' => $token,
                         'status' => __($status),
                     ], 200)
                     : response()->json([
